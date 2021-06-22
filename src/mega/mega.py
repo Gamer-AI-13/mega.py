dfdb256d1575ab8e98e1aeee62dd9d0cd12eeb54
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2021-06-22 19:57:51 +0530

    Update mega.py

--- src/mega/mega.py
+++ src/mega/mega.py
@@ -630,7 +630,7 @@ class Mega:
         nodes = self.get_files()
         return self.get_folder_link(nodes[node_id])
 
-    asnyc def download_url(self, url, dest_path=None, dest_filename=None):
+    async def download_url(self, url, dest_path=None, dest_filename=None):
         """
         Download a file by it's public url
         """
