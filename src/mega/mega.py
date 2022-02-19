2fe887a670cd50d25a03af536d30196b68d7853a
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2022-02-19 15:16:34 +0530

    test

--- src/mega/mega.py
+++ src/mega/mega.py
@@ -1024,6 +1024,52 @@ class Mega:
             return None
         result = {'size': size, 'name': unencrypted_attrs['n']}
         return result
+    def get_nodes_in_shared_folder(root_folder: str) -> dict:
+        data = [{"a": "f", "c": 1, "ca": 1, "r": 1}]
+        response = requests.post(
+            "https://g.api.mega.co.nz/cs",
+            params={'id': 0,  # self.sequence_num
+                    'n': root_folder},
+            data=json.dumps(data)
+        )
+        json_resp = response.json()
+        return json_resp[0]["f"]
+
+    def parse_folder_url(url: str) -> Tuple[str, str]:
+        "Returns (public_handle, key) if valid. If not returns None."
+        REGEXP1 = re.compile(r"mega.[^/]+/folder/([0-z-_]+)#([0-z-_]+)(?:/folder/([0-z-_]+))*")
+        REGEXP2 = re.compile(r"mega.[^/]+/#F!([0-z-_]+)[!#]([0-z-_]+)(?:/folder/([0-z-_]+))*")
+        m = re.search(REGEXP1, url)
+        if not m:
+            m = re.search(REGEXP2, url)
+        if not m:
+            print("Not a valid URL")
+            return None
+        root_folder = m.group(1)
+        key = m.group(2)
+        # You may want to use m.group(-1)
+        # to get the id of the subfolder
+        return (root_folder, key)
+
+    def decrypt_node_key(key_str: str, shared_key: str) -> Tuple[int, ...]:
+        encrypted_key = base64_to_a32(key_str.split(":")[1])
+        return decrypt_key(encrypted_key, shared_key)
+
+    def trynew_method(self, link):
+        (root_folder, shared_enc_key) = parse_folder_url(link)
+        shared_key = base64_to_a32(shared_enc_key)
+        nodes = get_nodes_in_shared_folder(root_folder)
+        for node in nodes:
+            key = decrypt_node_key(node["k"], shared_key)
+            if node["t"] == 0: # Is a file
+                k = (key[0] ^ key[4], key[1] ^ key[5], key[2] ^ key[6], key[3] ^ key[7])
+            elif node["t"] == 1: # Is a folder
+                k = key
+            attrs = decrypt_attr(base64_url_decode(node["a"]), k)
+            file_name = attrs["n"]
+            file_id = node["h"]
+            print(file_name)
+            print(file_id)
 
     def import_public_file(self,
                            file_handle,
